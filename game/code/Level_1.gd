541be5c73f3ae0c7689876a16f6f13cb210d6507
Manuel Rodriguez <valle.mrv@gmail.com>
2020-03-16 18:53:37 +0100

    Fixed random enemy
    Fixed random anemy, create scene path and can create inside player

--- game/code/Level_1.gd
+++ game/code/Level_1.gd
@@ -4,19 +4,12 @@ export (PackedScene) var Virus
 
 
 onready var player = $Player
-onready var position_ran = $Player/position_virus/position_ram
-onready var create_virus = $Timers/create_virus
 # Called when the node enters the scene tree for the first time.
 
-
-var _limite_visible = Vector2.ZERO
 
 
 func _ready():
 	randomize()
-	create_virus.start()
-	_limite_visible = get_viewport_rect().size
-
 
 
 func _on_Area2D_body_entered(body):
@@ -37,13 +30,3 @@ func _on_Player_die():
 
 func _on_Timer_timeout():
 	pass
-
-
-func _on_create_virus_timeout():
-	position_ran.set_unit_offset(randf())
-	var v = Virus.instance() 
-	v.global_position = position_ran.global_position
-	add_child(v)
-	#v.set_linear_velocity(Vector2(rand_range(v.SPEED_MIN, v.SPEED_MAX), 1))
-	create_virus.start()
-	print("now")
